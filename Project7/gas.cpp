5330d1773e2c9d7854f2d3f5456d78cfd6a76e8f
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2020-12-07 11:23:23 -0800

    revert main

--- Project7/gas.cpp
+++ Project7/gas.cpp
@@ -637,7 +637,7 @@ int main()
 {
     // Create a game
     // Use this instead to create a mini-game:   Game g(3, 4, 2);
-    Game g(MAXROWS, MAXCOLS, MAXFLATULANS);
+    Game g(7, 8, 25);
 
     // Play the game
     g.play();
